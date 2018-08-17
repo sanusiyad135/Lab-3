80c50af16d66db7a90d0cff15de8f650b85b995f
sanusiyad135 <42239567+sanusiyad135@users.noreply.github.com>
2018-08-17 17:16:15 +0000

    Create lab3_q1.cpp

---
+++ lab3_q1.cpp
@@ -0,0 +1,27 @@
+//first include library
+#include <iostream>
+using namespace std;
+// Write the main function
+int main() {
+          // Declaration variabled of all types
+          int x,y;//Declarating variabled of all types
+          float i;
+          char p;
+          bool m;
+          double q;
+          //Definig the variables
+          x = 10;
+          y = 5;
+          i = 3.8;
+          p = 'b';
+          m = 1;
+          q = 1.6;
+          //Displaying the sizes of datatypes
+          cout <<"Sizes of x=" << sizeof(x) <<endl;
+          cout <<"Sizes of i=" << sizeof(i) <<endl;
+	  cout <<"Sizes of p=" << sizeof(p) <<endl;
+	  cout <<"Sizes of m=" << sizeof(m) <<endl;
+	  cout <<"Sizes of q=" << sizeof(q) <<endl;
+	  cout <<"Sizes of y=" << sizeof(y) <<endl;
+return 0;
+}
