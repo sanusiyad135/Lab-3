702cd91c1a9a35e8f3d8ca7215ad41b881e03ac3
sanusiyad135 <42239567+sanusiyad135@users.noreply.github.com>
2018-08-17 22:13:15 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+// decalring the variables
+int x,y,a,s,m,d,r;
+cout<<"Enter the first number"<<endl;
+cin>>x;
+cout<<"enter the second number"<<endl;
+cin>>y;
+// command for adding
+a = x+y;
+// command for substracting
+s = x-y;
+// comand for multiplication
+m = x*y;
+// command for deviding
+d = x/y;
+// command for finding the reminder
+r = x%y;
+cout<<"the sum is "<<a<<endl<<"The Difference is "<<s<<endl<<"The product "<<m<<endl<<"The reminder is "<<r<<endl;
+
+
+return 0;
+}
